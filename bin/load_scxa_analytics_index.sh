a3fb878e44ca14469a66886d20963810bb3d3c58
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-05-16 10:19:22 +0100

    bumps version of schema

--- bin/load_scxa_analytics_index.sh
+++ bin/load_scxa_analytics_index.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export SCHEMA_VERSION=1
+export SCHEMA_VERSION=2
 export SOLR_COLLECTION=scxa-analytics-v$SCHEMA_VERSION
 
 echo "Loading cond. sdrf $CONDENSED_SDRF_TSV into host $SOLR_HOST collection $SOLR_COLLECTION..."
