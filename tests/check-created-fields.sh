1471215604795ca67ad02677a1c8eeec88ea6d47
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-05-16 21:17:28 +0100

    Cleans up debugging for travis.

--- tests/check-created-fields.sh
+++ tests/check-created-fields.sh
@@ -8,23 +8,15 @@ HOST=${SOLR_HOST:-"localhost:8983"}
 CORE=${SOLR_COLLECTION:-"scxa-analytics-v$SCHEMA_VERSION"}
 
 echo "Retrieving fields in the schema"
-curl "http://$HOST/solr/$CORE/schema?wt=json"
 curl "http://$HOST/solr/$CORE/schema?wt=json" \
   | jq '.schema.fields + .schema.dynamicFields | .[].name ' | sed s/\"//g \
   | grep -v '^\*' | grep -v '^_' | grep -v '^id$' | grep -v '^attr_\*$' \
   | sort > loaded_fields.txt
 
-ls -l loaded_fields.txt
 echo "Parsing creationg script"
-ls -l bin/create-scxa-analytics-schema.sh
 grep -A 2 '\("add-field"\|"add-dynamic-field"\)' bin/create-scxa-analytics-schema.sh \
   | grep '"name"' | awk -F':' '{ print $2 }' | sed 's/[\", ]//g' \
   | sort > expected_loaded_fields.txt
 
-echo "Expected"
-cat expected_loaded_fields.txt
-echo "Actual"
-cat loaded_fields.txt
 echo "Running cmp"
 cmp loaded_fields.txt expected_loaded_fields.txt
-echo "Done"
