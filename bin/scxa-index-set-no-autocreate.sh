e7608840ece61dd6443c344e877c6346c5d914ab
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-06-15 17:55:01 +0100

    Removes explicit project duplication from set-no-autocreate.

--- bin/scxa-index-set-no-autocreate.sh
+++ bin/scxa-index-set-no-autocreate.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-SCHEMA_VERSION=2
+SCHEMA_VERSION=${SCHEMA_VERSION:-2}
 
 # on developers environment export SOLR_HOST_PORT and export SOLR_COLLECTION before running
 HOST=${SOLR_HOST:-"localhost:8983"}
 CORE=${SOLR_COLLECTION:-"scxa-analytics-v$SCHEMA_VERSION"}
 
-
-curl -X POST -H 'Content-type:application/json' --data-binary '{
-  "set-user-property": {"update.autoCreateFields":"false"}
-}' http://$HOST/solr/$CORE/config
-
-SCHEMA_VERSION=1
-CORE=${SOLR_COLLECTION:-"scxa-gene2experiment-v$SCHEMA_VERSION"}
 
 curl -X POST -H 'Content-type:application/json' --data-binary '{
   "set-user-property": {"update.autoCreateFields":"false"}
