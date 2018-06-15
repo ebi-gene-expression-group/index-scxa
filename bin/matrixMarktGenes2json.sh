b647711cdef85b7ea106bd1d74a78438879eea43
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-06-16 00:26:15 +0100

    Missing input-output generator

--- bin/matrixMarktGenes2json.sh
+++ bin/matrixMarktGenes2json.sh
@@ -2,4 +2,4 @@
 
 awk -v EXP_ID="$EXP_ID" \
     '{ print "{ \"experiment_accession\": \""EXP_ID"\",\n \"gene_id\": \""$2"\" }" }' \
-       $MATRIX_MARKT_ROWS_GENES_FILE | jq -s
+       $MATRIX_MARKT_ROWS_GENES_FILE | jq -s .
