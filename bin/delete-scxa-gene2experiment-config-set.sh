fb433b37c7d015b197bcca0a2d38d52834268a48
Pablo Moreno <pablo.a.moreno@gmail.com>
2018-06-18 16:28:40 +0100

    Fixes version for config delete (gene2experiments)

--- bin/delete-scxa-gene2experiment-config-set.sh
+++ bin/delete-scxa-gene2experiment-config-set.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-export SCHEMA_VERSION=2
+export SCHEMA_VERSION=1
 export SOLR_COLLECTION=scxa-gene2experiment-v$SCHEMA_VERSION
 HOST=${SOLR_HOST:-localhost:8983}
 CONFIG=$SOLR_COLLECTION
