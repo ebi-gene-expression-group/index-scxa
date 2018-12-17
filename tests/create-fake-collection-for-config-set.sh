2c46f2ab2ee13d029fb78b88620866393947aa7c
Monica Jianu <monica@ebi.ac.uk>
2018-12-17 11:55:28 +0000

    Enabled cloud mode for Docker Solr in Travis config. Added flag to enable runtime libraries for BioSolr. Fixed collection creation command in test script to pass number of shards, which is required for Solr cloud mode.

--- tests/create-fake-collection-for-config-set.sh
+++ tests/create-fake-collection-for-config-set.sh
@@ -4,4 +4,4 @@ set -e
 HOST=${SOLR_HOST:-"localhost:8983"}
 
 printf "\n\nCreating fake collection on $HOST to obtain a _default initial config"
-curl "http://$HOST/solr/admin/collections?action=CREATE&name=fakeCollection"
+curl "http://$HOST/solr/admin/collections?action=CREATE&name=fakeCollection&numShards=1"
