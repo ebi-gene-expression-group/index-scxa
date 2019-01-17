d0fccd9cbb3db758c37a00d48337bafba1fb4066
Pablo Moreno <pablo.a.moreno@gmail.com>
2019-01-17 18:55:21 +0000

    Fail if curl request for loading data doesn't return a 2xx HTTP code

--- bin/loadJSONIndexToSolr.sh
+++ bin/loadJSONIndexToSolr.sh
@@ -14,4 +14,14 @@ elif [ $ONTOLOGY_PROCESSOR ]; then
 	PROCESSOR="&processor="$ONTOLOGY_PROCESSOR
 fi
 
-curl "http://$HOST/solr/$COLLECTION/update?commit=true$PROCESSOR" --data-binary @- -H 'Content-type:application/json'
+#creates a new file descriptor 3 that redirects to 1 (STDOUT)
+exec 3>&1
+# Run curl in a separate command, capturing output of -w "%{http_code}" into HTTP_STATUS
+# and sending the content to this command's STDOUT with -o >(cat >&3)
+HTTP_STATUS=$(curl -w "%{http_code}" -o >(cat >&3) "http://$HOST/solr/$COLLECTION/update?commit=true$PROCESSOR" --data-binary @- -H 'Content-type:application/json')
+
+if [[ ! $HTTP_STATUS == 2* ]];
+then
+	 # HTTP Status is not a 2xx code, so it is an error.
+   exit 1
+fi
