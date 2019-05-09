e248c48bc417fd862c3878e9a4a4d9dae2d262fe
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2019-05-09 15:26:00 +0100

    Set maxBooleanClauses to a sufficiently high value

--- bin/create-scxa-analytics-config-set.sh
+++ bin/create-scxa-analytics-config-set.sh
@@ -10,3 +10,14 @@ BASE=${SOLR_BASE_CONFIG:-"_default"}
 
 printf "\n\nCreating config based on $BASE for our collection."
 curl "http://$HOST/solr/admin/configs?action=CREATE&name=$CORE&baseConfigSet=$BASE"
+
+# Set this value to whatever is needed, it doesn’t really much with current Lucene versions
+# https://issues.apache.org/jira/browse/SOLR-4586
+MAX_BOOLEAN_CLAUSES=16384
+printf "\n\nRaising value of maxBooleanClauses to $MAX_BOOLEAN_CLAUSES."
+curl "http://$HOST/solr/$CORE/config" -H 'Content-type:application/json' -d "
+{
+  "set-property": {
+    "query.maxBooleanClauses" : ${MAX_BOOLEAN_CLAUSES}
+  }
+}"
