e3bb4f442e87d8be465cc45856bbd7dfcfaa3e9c
amunozpomer <amunoz@ebi.ac.uk>
2020-12-11 16:14:37 +0000

    Remove fields and config settings which have a sensible default

--- bin/create-scxa-analytics-schema.sh
+++ bin/create-scxa-analytics-schema.sh
@@ -67,48 +67,6 @@ curl -X POST -H 'Content-type:application/json' --data-binary '{
 }' http://$HOST/solr/$CORE/schema
 
 #############################################################################################
-
-printf "\n\nDelete field ontology_synonyms "
-curl -X POST -H 'Content-type:application/json' --data-binary '{
-  "delete-field":
-  {
-    "name": "ontology_synonyms"
-  }
-}' http://$HOST/solr/$CORE/schema
-
-printf "\n\nCreate ontology_synonyms (string, multiValued, docValues) "
-curl -X POST -H 'Content-type:application/json' --data-binary '{
-  "add-field":
-  {
-    "name": "ontology_synonyms",
-    "type": "string",
-    "multiValued": true,
-    "docValues": true
-  }
-}' http://$HOST/solr/$CORE/schema
-
-#############################################################################################
-
-printf "\n\nDelete field ontology_definition "
-curl -X POST -H 'Content-type:application/json' --data-binary '{
-  "delete-field":
-  {
-    "name": "ontology_definition"
-  }
-}' http://$HOST/solr/$CORE/schema
-
-printf "\n\nCreate ontology_synonyms (string, multiValued) "
-curl -X POST -H 'Content-type:application/json' --data-binary '{
-  "add-field":
-  {
-    "name": "ontology_definition",
-    "type": "string",
-    "multiValued": true,
-    "docValues": true
-  }
-}' http://$HOST/solr/$CORE/schema
-
-#############################################################################################
 # Deletion of copy field needs to come before the deletion of the actual fields.
 # 1.1
 printf "\n\nDelete copy field rule for facet_factor_* "
@@ -354,12 +312,7 @@ curl -X POST -H 'Content-type:application/json' --data-binary '{
     "name": "'$CORE'_ontology_expansion"
     "runtimeLib": true,
     "class": "uk.co.flax.biosolr.solr.update.processor.OntologyUpdateProcessorFactory",
-    "enabled": "true",
     "annotationField": "ontology_annotation",
-    "synonymsField": "ontology_synonyms",
-    "definitionField": "",
-    "childField": "",
-    "descendantsField": "",
     "ontologyURI": "https://raw.githubusercontent.com/EBISPOT/scatlas_ontology/zooma_file_proc_release/scatlas.owl",
     "includeChildren": false,
     "includeDescendants": false
