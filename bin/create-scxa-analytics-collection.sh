b967d7a5ff8ff4013675652638d9f3e4e925ce2b
amunozpomer <amunoz@ebi.ac.uk>
2020-12-14 10:10:18 +0000

    Rename variable $CORE to $COLLECTION

--- bin/create-scxa-analytics-collection.sh
+++ bin/create-scxa-analytics-collection.sh
@@ -5,19 +5,19 @@ set -e
 
 # on developers environment export SOLR_HOST_PORT and export SOLR_COLLECTION before running
 HOST=${SOLR_HOST:-"localhost:8983"}
-CORE=${SOLR_COLLECTION:-"scxa-analytics-v$SCHEMA_VERSION"}
+COLLECTION=${SOLR_COLLECTION:-"scxa-analytics-v$SCHEMA_VERSION"}
 NUM_SHARDS=${SOLR_NUM_SHARDS:-1}
 REPLICATES=${SOLR_REPLICATES:-1}
 MAX_SHARDS_PER_NODE=${SOLR_MAX_SHARDS_PER_NODE:-1}
 
-printf "\n\nCreating collection $CORE based on $HOST"
-curl "http://$HOST/solr/admin/collections?action=CREATE&name=$CORE&numShards=$NUM_SHARDS&replicationFactor=$REPLICATES&maxShardsPerNode=$MAX_SHARDS_PER_NODE"
+printf "\n\nCreating collection $COLLECTION based on $HOST"
+curl "http://$HOST/solr/admin/collections?action=CREATE&name=$COLLECTION&numShards=$NUM_SHARDS&replicationFactor=$REPLICATES&maxShardsPerNode=$MAX_SHARDS_PER_NODE"
 
 # Set this value to whatever is needed, it doesn’t really matter with current Lucene versions
 # https://issues.apache.org/jira/browse/SOLR-4586
 MAX_BOOLEAN_CLAUSES=100000000
 printf "\n\nRaising value of maxBooleanClauses to $MAX_BOOLEAN_CLAUSES."
-curl "http://$HOST/solr/$CORE/config" -H 'Content-type:application/json' -d "
+curl "http://$HOST/solr/$COLLECTION/config" -H 'Content-type:application/json' -d "
 {
   "set-property": {
     "query.maxBooleanClauses" : ${MAX_BOOLEAN_CLAUSES}
