f089077e84199bc48ed39ad0bb6a7a1f17aaa4a7
upendra kumbham <ukumbham@ebi.ac.uk>
2021-05-10 13:53:35 +0100

    Fix review comments

--- bin/build-scxa-analytics-suggestions.sh
+++ bin/build-scxa-analytics-suggestions.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+SCHEMA_VERSION=5
 
 #This below long curl request build suggestions on the solr server for the mentioned dictionaries as part of http request
 #Once build request completes, user can search for the suggestions
 # To search for the suggestions, here is the curl request:
 # curl -X GET 'http://localhost:8983/solr/scxa-analytics-v5/suggest?suggest=true&suggest.dictionary=ontologyAnnotationSuggester&suggest.dictionary=ontologyAnnotationAncestorSuggester&suggest.dictionary=ontologyAnnotationParentSuggester&suggest.dictionary=ontologyAnnotationSynonymSuggester&suggest.dictionary=ontologyAnnotationChildSuggester&suggest.q=skin'
 
-curl -X GET 'http://localhost:8983/solr/scxa-analytics-v5/suggest?suggest=true&suggest.build=true&suggest.dictionary=ontologyAnnotationSuggester&suggest.dictionary=ontologyAnnotationAncestorSuggester&suggest.dictionary=ontologyAnnotationParentSuggester&suggest.dictionary=ontologyAnnotationSynonymSuggester&suggest.dictionary=ontologyAnnotationChildSuggester'
+# on developers environment export SOLR_HOST and export SOLR_COLLECTION before running
+HOST=${SOLR_HOST:-"localhost:8983"}
+COLLECTION=${SOLR_COLLECTION:-"scxa-analytics-v${SCHEMA_VERSION}"}
+
+curl "http://$HOST/solr/$COLLECTION/suggest?suggest=true&suggest.build=true&suggest.dictionary=ontologyAnnotationSuggester&suggest.dictionary=ontologyAnnotationAncestorSuggester&suggest.dictionary=ontologyAnnotationParentSuggester&suggest.dictionary=ontologyAnnotationSynonymSuggester&suggest.dictionary=ontologyAnnotationChildSuggester"
 
