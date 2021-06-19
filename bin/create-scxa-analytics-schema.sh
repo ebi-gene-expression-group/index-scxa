91bed2d7746a132d9c5a910ee74d05e777b96354
amunozpomer <amunoz@ebi.ac.uk>
2021-06-19 16:52:10 +0100

    Bump scxa-analytics version

--- bin/create-scxa-analytics-schema.sh
+++ bin/create-scxa-analytics-schema.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-SCHEMA_VERSION=5
+SCHEMA_VERSION=6
 
 # on developers environment export SOLR_HOST_PORT and export SOLR_COLLECTION before running
 HOST=${SOLR_HOST:-"localhost:8983"}
