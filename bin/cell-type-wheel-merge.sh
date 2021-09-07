32c54eb940651d8a613876f3d7aa5d4ca5648ac7
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-09-07 17:41:37 +0100

    Fix typo in comment

--- bin/cell-type-wheel-merge.sh
+++ bin/cell-type-wheel-merge.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Add properties from object in $2 whose key matches the filed cell_id in $1
+# Add properties from object in $2 whose key matches the field cell_id in $1
 jq -c \
 --slurpfile ctw $2 \
 '
