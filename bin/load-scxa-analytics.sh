5b7ae4665f4b37e686d4ae55de171495cf9ef12a
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-09-07 15:22:12 +0100

    Rename files and reword comments to reflect changes in the way we add the new CTW fields

--- bin/load-scxa-analytics.sh
+++ bin/load-scxa-analytics.sh
@@ -4,8 +4,8 @@ set -e
 [ -z ${CONDENSED_SDRF_TSV+x} ] && echo "CONDENSED_SDRF_TSV env var is needed." && exit 1
 
 cleanup() {
-  echo "Cleaning up all JSONL files"
-  rm $COND_SDRF_JSONL_FILENAME $CTW_HELPER_JSONL_FILENAME $CTW_ENRICHED_COND_SDRF_JSONL_FILENAME
+  echo "Cleaning up all JSON/JSONL files"
+  #rm $COND_SDRF_JSONL_FILENAME $CTW_HELPER_JSON_FILENAME $CTW_ENRICHED_COND_SDRF_JSONL_FILENAME
 }
 trap cleanup exit
 
@@ -19,15 +19,16 @@ BASE_FILENAME=`basename $CONDENSED_SDRF_TSV .tsv`
 
 COND_SDRF_JSONL_FILENAME=$WORKDIR/$BASE_FILENAME.jsonl
 echo "Transform condensed SDRF file to JSONL -> $COND_SDRF_JSONL_FILENAME"
-condSdrf2tsvForSCXAJSONFactorsIndex.sh $CONDENSED_SDRF_TSV > $COND_SDRF_JSONL_FILENAME
+condSdrf2tsvForSCXAJSONFactorsIndex.sh $CONDENSED_SDRF_TSV | jq -c > $COND_SDRF_JSONL_FILENAME
 
-CTW_HELPER_JSONL_FILENAME=$WORKDIR/$BASE_FILENAME.cell-type-wheel.jsonl
-echo "Creating cell type wheel helper file -> $CTW_HELPER_JSONL_FILENAME"
-cell-type-wheel-cond-sdrf-to-ctw-fields.sh $COND_SDRF_JSONL_FILENAME > $CTW_HELPER_JSONL_FILENAME
+CTW_HELPER_JSON_FILENAME=$WORKDIR/$BASE_FILENAME.cell-type-wheel.json
+echo "Creating cell type wheel helper dictionary JSON file -> $CTW_HELPER_JSON_FILENAME"
+cell-type-wheel-cond-sdrf-to-ctw-fields.sh $COND_SDRF_JSONL_FILENAME > $CTW_HELPER_JSON_FILENAME
 
 CTW_ENRICHED_COND_SDRF_JSONL_FILENAME=$WORKDIR/$BASE_FILENAME.ctw-enriched.jsonl
-echo "Merging cell type wheel fields to create final JSONL file -> $CTW_ENRICHED_COND_SDRF_JSONL_FILENAME"
-cell-type-wheel-merge.sh $CTW_HELPER_JSONL_FILENAME $COND_SDRF_JSONL_FILENAME | jsonl-filter-empty-string-values.sh > $CTW_ENRICHED_COND_SDRF_JSONL_FILENAME
+echo "Adding cell type wheel fields to create final JSONL file -> $CTW_ENRICHED_COND_SDRF_JSONL_FILENAME"
+cell-type-wheel-merge.sh $COND_SDRF_JSONL_FILENAME $CTW_HELPER_JSON_FILENAME | jsonl-filter-empty-string-values.sh > $CTW_ENRICHED_COND_SDRF_JSONL_FILENAME
 
 export INPUT_JSONL=$CTW_ENRICHED_COND_SDRF_JSONL_FILENAME
 solr-jsonl-chunk-loader.sh
+
