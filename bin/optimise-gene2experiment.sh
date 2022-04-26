4ef2c12de7a38f1051262d0283592619f541bfaa
Pablo Moreno <pcm32@users.noreply.github.com>
2022-04-26 15:04:48 +0100

    Script and tests for collections optimisations (#34)
    * Script and tests for collections optimisations
    
    * Separate processes
    
    * Fix typos
    
    * Badly set versions

---
+++ bin/optimise-gene2experiment.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+SCHEMA_VERSION=1
+
+# On developers environment export SOLR_HOST and export SOLR_COLLECTION before running
+HOST=${SOLR_HOST:-"localhost:8983"}
+COLLECTION=${SOLR_COLLECTION:-"scxa-gene2experiment-v$SCHEMA_VERSION"}
+
+# creates a new file descriptor 3 that redirects to 1 (STDOUT)
+exec 3>&1
+
+echo "Optimising $COLLECTION..."
+HTTP_STATUS=$(curl -w "%{http_code}" -o >(cat >&3) -s "http://${HOST}/solr/${COLLECTION}/update?optimize=true")
+
+if [[ ! $HTTP_STATUS == 2* ]]; then
+   # HTTP Status is not a 2xx code
+   echo "Faiure on optimising collection $coll"
+   exit 1
+fi
