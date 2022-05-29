2ca64a4999983ea6dfbd5cd8ee80980eaf0bd48e
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-05-29 12:38:24 +0100

    Source scxa-analytics schema version in a single environment file

--- tests/analytics-check-index-content.sh
+++ tests/analytics-check-index-content.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. ${DIR}/../scxa-analytics-schema-version.env
+
 set -e
-SCHEMA_VERSION=6
 
 # on developers environment export SOLR_HOST_PORT and export SOLR_COLLECTION before running
 HOST=${SOLR_HOST:-"localhost:8983"}
