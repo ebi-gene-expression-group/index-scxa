bde9991afc5fba74b8ce7c7f94075774216148ae
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2022-05-29 12:43:06 +0100

    Add a script that checks for the presence of a non-empty payloadField in suggestions

---
+++ tests/analytics-check-suggesters-payload.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+. ${DIR}/../scxa-analytics-schema-version.env
+
+HOST=${SOLR_HOST:-"localhost:8983"}
+COLLECTION=${SOLR_COLLECTION:-"scxa-analytics-v${SCHEMA_VERSION}"}
+
+REQUEST_URI="http://$HOST/solr/$COLLECTION/suggest?suggest=true"
+TEST_QUERY=lymph
+
+set -e
+for SUGGESTER in ontologyAnnotationSuggester ontologyAnnotationAncestorSuggester ontologyAnnotationParentSuggester ontologyAnnotationSynonymSuggester; do
+  echo "$REQUEST_URI&suggest.dictionary=$SUGGESTER&suggest.q=$TEST_QUERY"
+  curl -X GET "$REQUEST_URI&suggest.dictionary=$SUGGESTER&suggest.q=$TEST_QUERY" | \
+  # Check that all suggestions have a non-empty payload
+  jq -e ".suggest.$SUGGESTER.$TEST_QUERY.suggestions[] | select (.payload != \"\")" > /dev/null
+done
+
+exit 0
