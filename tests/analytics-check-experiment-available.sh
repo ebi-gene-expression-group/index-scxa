3a44e630fbe4529acc045dd54589d2cee9580b11
Pablo Moreno <pablo.a.moreno@gmail.com>
2022-05-29 16:06:59 +0100

    Try to see output before jq

--- tests/analytics-check-experiment-available.sh
+++ tests/analytics-check-experiment-available.sh
@@ -10,7 +10,7 @@ SOLR_USER=${SOLR_USER:-"solr"}
 SOLR_PASS=${SOLR_PASS:-"SolrRocks"}
 SOLR_AUTH="-u $SOLR_USER:$SOLR_PASS"
 
-entries=$(curl $SOLR_AUTH "http://$HOST/solr/$CORE/select?fl=experiment_accession&q=experiment_accession:%22$EXP_ID%22" |   jq '.response.numFound')
+entries=$(curl $SOLR_AUTH "http://$HOST/solr/$CORE/select?fl=experiment_accession&q=experiment_accession:%22$EXP_ID%22" | tee /dev/stdout |  jq '.response.numFound')
 
 if [ $entries -lt 1 ]; then
   echo "$EXP_ID not present in index $CORE at $HOST"
