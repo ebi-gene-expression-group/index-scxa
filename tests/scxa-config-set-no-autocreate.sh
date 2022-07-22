115b722dc53608b0743208d81fcccb1947ebcb6d
Pablo Moreno <pcm32@users.noreply.github.com>
2022-07-22 16:58:11 +0100

    Solr auth on scxa-config-set-no-auto-create

--- tests/scxa-config-set-no-autocreate.sh
+++ tests/scxa-config-set-no-autocreate.sh
@@ -3,7 +3,10 @@
 # on developers environment export SOLR_HOST_PORT and export SOLR_COLLECTION before running
 HOST=${SOLR_HOST:-"localhost:8983"}
 CORE=${SOLR_COLLECTION}
+SOLR_USER=${SOLR_USER:-"solr"}
+SOLR_PASS=${SOLR_PASS:-"SolrRocks"}
+SOLR_AUTH="-u $SOLR_USER:$SOLR_PASS"
 
 curl -X POST -H 'Content-type:application/json' --data-binary '{
   "set-user-property": {"update.autoCreateFields":"false"}
-}' "http://$HOST/solr/$CORE/config"
+}' $SOLR_AUTH "http://$HOST/solr/$CORE/config"
