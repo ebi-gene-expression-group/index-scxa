ad418976772b381a8a848b1e5c66f93d7fe2f9c3
Pablo Moreno <pablo.a.moreno@gmail.com>
2022-07-25 08:43:06 +0100

    Delete/create collections and create aliases

--- bin/create-scxa-analytics-collection.sh
+++ bin/create-scxa-analytics-collection.sh
@@ -14,6 +14,12 @@ SOLR_USER=${SOLR_USER:-"solr"}
 SOLR_PASS=${SOLR_PASS:-"SolrRocks"}
 SOLR_AUTH="-u $SOLR_USER:$SOLR_PASS"
 
+printf "\n\nDeleting alias for collection\n"
+curl $SOLR_AUTH "http://$HOST/solr/admin/collections?action=DELETEALIAS&name=scxa-analytics"
+
+printf "\n\nDeleting collection ${COLLECTION} based on ${HOST}\n"
+curl $SOLR_AUTH "http://$HOST/solr/admin/collections?action=DELETE&name=$COLLECTION"
+
 printf "\n\nCreating collection $COLLECTION on $HOST"
 curl $SOLR_AUTH "http://$HOST/solr/admin/collections?action=CREATE&name=$COLLECTION&numShards=$NUM_SHARDS&replicationFactor=$REPLICATES&maxShardsPerNode=$MAX_SHARDS_PER_NODE"
 
@@ -52,3 +58,7 @@ curl $SOLR_AUTH "http://$HOST/solr/$COLLECTION/config" -H 'Content-type:applicat
     "updateHandler.autoSoftCommit.maxDocs":-1
   }
 }'
+
+printf "\n\nCreating collection ${COLLECTION} alias scxa-analytics\n"
+curl $SOLR_AUTH "http://${HOST}/solr/admin/collections?action=CREATEALIAS&name=scxa-analytics&collections=${COLLECTION}"
+
--- bin/create-scxa-gene2experiment-collection.sh
+++ bin/create-scxa-gene2experiment-collection.sh
@@ -5,7 +5,7 @@ set -e
 
 # on developers environment export SOLR_HOST_PORT and export SOLR_COLLECTION before running
 HOST=${SOLR_HOST:-"localhost:8983"}
-CORE=${SOLR_COLLECTION:-"scxa-gene2experiment-v$SCHEMA_VERSION"}
+COLLECTION=${SOLR_COLLECTION:-"scxa-gene2experiment-v$SCHEMA_VERSION"}
 NUM_SHARDS=${SOLR_NUM_SHARDS:-1}
 REPLICATES=${SOLR_REPLICATES:-1}
 MAX_SHARDS_PER_NODE=${SOLR_MAX_SHARDS_PER_NODE:-1}
@@ -13,5 +13,14 @@ SOLR_USER=${SOLR_USER:-"solr"}
 SOLR_PASS=${SOLR_PASS:-"SolrRocks"}
 SOLR_AUTH="-u $SOLR_USER:$SOLR_PASS"
 
+printf "\n\nDeleting alias for collection\n"
+curl $SOLR_AUTH "http://${HOST}/solr/admin/collections?action=DELETEALIAS&name=scxa-gene2experiment"
+
+printf "\n\nDeleting collection ${COLLECTION} based on ${HOST}\n"
+curl $SOLR_AUTH "http://${HOST}/solr/admin/collections?action=DELETE&name=${COLLECTION}"
+
 printf "\n\nCreating collection $CORE based on $HOST"
-curl $SOLR_AUTH "http://$HOST/solr/admin/collections?action=CREATE&name=$CORE&collection.configName=$CORE&numShards=$NUM_SHARDS&replicationFactor=$REPLICATES&maxShardsPerNode=$MAX_SHARDS_PER_NODE"
+curl $SOLR_AUTH "http://$HOST/solr/admin/collections?action=CREATE&name=$COLLECTION&collection.configName=$COLLECTION&numShards=$NUM_SHARDS&replicationFactor=$REPLICATES&maxShardsPerNode=$MAX_SHARDS_PER_NODE"
+
+printf "\n\nCreating collection ${COLLECTION} alias scxa-gene2experiment\n"
+curl $SOLR_AUTH "http://${HOST}/solr/admin/collections?action=CREATEALIAS&name=scxa-gene2experiment&collections=${COLLECTION}"
