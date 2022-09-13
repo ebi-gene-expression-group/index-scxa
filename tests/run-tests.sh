08490fa5e8b4afc9749ec04d783986a2c090b91f
Pablo Moreno <pablo.a.moreno@gmail.com>
2022-09-13 11:07:22 +0100

    Revert "Update run-tests.sh"
    This reverts commit e04eae22cef8f6b32530a13837534598c1f2d2f2.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -2,9 +2,6 @@
 # Sets up the environment and calls [BATS](https://github.com/sstephenson/bats) with provided arguments
 source "$(dirname "${BASH_SOURCE[0]}")"/runner-setup.sh
 
-# to check why we cannot write to /tmp
-ls -l /
-
 if [ "$#" -eq 0 ]; then
 	bats --tap "$(dirname "${BASH_SOURCE[0]}")"
 else
